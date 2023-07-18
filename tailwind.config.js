23cb31ad7f5b25716e5a8258e48ea48b26ff20cb
elvis-ndubuisi <provictor.ie@gmail.com>
2023-07-18 09:04:32 +0100

    🎨updated tailwind.config with extra colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontSize: {
+      'text-base': '15px'
+    },
+    extend: {
+      colors: {
+        'ef-text': '#050505',
+        'ef-bg': '#fafafa',
+        'ef-primary': '#8fb3ff',
+        'ef-secondary': '#ccdbff',
+        'ef-accent': '#e11e72'
+      }
+    }
   },
   plugins: []
 }
